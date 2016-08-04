93e64782288974a84ec3ef8810635d08b5dd1f4b
Boyko Karadzhov <bokovskii@gmail.com>
2016-08-04 22:50:32 +0300

    added nuspec and tests project

---
+++ Karadzhov.DecayingCollections.Tests/DecayingCollectionTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Karadzhov.DecayingCollections.Tests
+{
+    [TestClass]
+    public class DecayingCollectionTests
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void Constructor_NullTimer_Throws()
+        {
+            using (new SampleDecayingCollection(null, 0, 0)) { }
+        }
+    }
+}
---
+++ Karadzhov.DecayingCollections.Tests/SampleDecayingCollection.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Karadzhov.DecayingCollections.Tests
+{
+    public class SampleDecayingCollection : DecayingCollection<object, List<object>>
+    {
+        public SampleDecayingCollection(int lifespanInSeconds) : base(lifespanInSeconds)
+        {
+        }
+
+        public SampleDecayingCollection(int lifespanInSeconds, int steps) : base(lifespanInSeconds, steps)
+        {
+        }
+
+        public SampleDecayingCollection(ITimer timer, int lifespanInSeconds, int steps) : base(timer, lifespanInSeconds, steps)
+        {
+        }
+    }
+}
