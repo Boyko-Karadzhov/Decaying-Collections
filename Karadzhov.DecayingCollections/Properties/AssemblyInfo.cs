594c847fd7e589ae354981814510d5f37726747e
Boyko Karadzhov <bokovskii@gmail.com>
2016-08-14 16:43:49 +0300

    fixed version mismatch

--- Karadzhov.DecayingCollections/Properties/AssemblyInfo.cs
+++ Karadzhov.DecayingCollections/Properties/AssemblyInfo.cs
@@ -32,7 +32,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.2.0.14")]
-[assembly: AssemblyFileVersion("0.2.0.14")]
+[assembly: AssemblyVersion("0.2.0.16")]
+[assembly: AssemblyFileVersion("0.2.0.16")]
 
 [assembly: CLSCompliant(true)]
